8a1fc723e2cc701a7861877c4626486027e4d271
moetez96 <moetez.ayari@esprit.tn>
2024-09-18 17:19:37 +0100

    Add logout button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {WebSocketService} from "../../socket/WebSocketService";
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(private authService: AuthService, private webSocketService: WebSocketService) {
+  }
+  handleLogout() {
+    this.authService.logout();
+    this.webSocketService.disconnect();
+    window.location.reload();
+  }
 }
